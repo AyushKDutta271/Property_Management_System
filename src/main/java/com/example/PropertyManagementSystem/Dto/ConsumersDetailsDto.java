dff052e4fe41351c8a9ac548577676358a69fe02
Ayush Kamal Dutta <duttaayush732@gmail.com>
2026-02-16 23:59:44 +0530

    Implemented logging with logback and SLF4J framework

--- src/main/java/com/example/PropertyManagementSystem/Dto/ConsumersDetailsDto.java
+++ src/main/java/com/example/PropertyManagementSystem/Dto/ConsumersDetailsDto.java
@@ -3,18 +3,32 @@ package com.example.PropertyManagementSystem.Dto;
 import com.example.PropertyManagementSystem.CATEGORY;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
 @Setter
 public class ConsumersDetailsDto {
+    @NotNull(message="first name is mandatory")
     private String firstName;
+
+    @NotNull(message="last name is mandatory")
     private String lastName;
+
+    @NotNull(message="Email is mandatory")
     private String emailId;
+
+    @NotNull(message="Password is mandatory")
+    @Size(min=5,message="Please enter password of minimum length 5")
     private String password;
+
+    @NotNull(message="please make sure you've permission")
     private Boolean permission;
 
-
+    @NotNull(message="Please choose category")
     private CATEGORY category;
+
+    @NotNull(message = "Enter property-id if you're seller")
     private Long propertyId;
 }
--- src/main/java/com/example/PropertyManagementSystem/Dto/PropertyDetailsDto.java
+++ src/main/java/com/example/PropertyManagementSystem/Dto/PropertyDetailsDto.java
@@ -2,13 +2,20 @@ package com.example.PropertyManagementSystem.Dto;
 
 import com.example.PropertyManagementSystem.PropertyType;
 import com.example.PropertyManagementSystem.STATUS;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 @Getter
 @Setter
 public class PropertyDetailsDto {
+    @NotNull(message="Every property must have a seller")
     private Long sellerId;
+
+    @NotNull(message="Please choose the status")
     private STATUS status;
+
+    @NotNull(message="choose property type")
     private PropertyType type;
+
     private Long ownerId;
 }
